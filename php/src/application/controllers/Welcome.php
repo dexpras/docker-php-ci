25dd1e29074967e2b30e968ae2816ab129d76b72
prasannademel <prasannademel29@gmail.com>
2024-03-06 00:07:37 +0530

    Removed db select

--- php/src/application/controllers/Welcome.php
+++ php/src/application/controllers/Welcome.php
@@ -21,7 +21,7 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('welcome_message');
-		$this->load->view('users', array('users' => $this->db->get('users')->result()));
+		// $this->load->view('users', array('users' => $this->db->get('users')->result()));
 		$this->load->view('test');
 	}
 }
